be19de53650fee80f09e41ea8135c2b98d851e67
jlozper <jonlozanop@gmail.com>
2020-05-17 09:12:12 +0200

    comments added

--- src/app/ui/autocomplete-tree/autocomplete-tree.component.ts
+++ src/app/ui/autocomplete-tree/autocomplete-tree.component.ts
@@ -5,6 +5,7 @@ import { Observable, BehaviorSubject } from 'rxjs';
 import { withLatestFrom, map } from 'rxjs/operators';
 
 import { TreeNode } from 'primeng/api/treenode';
+
 import { TreeEvent } from './models/tree-event.model';
 import { AutocompleteEvent } from './models/autocomplete-event.model';
 
@@ -15,13 +16,16 @@ import { AutocompleteEvent } from './models/autocomplete-event.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AutocompleteTreeComponent  implements ControlValueAccessor, OnInit {
-  @Input() listData$: Observable<TreeNode[]>;
+
+  @Input() listData$: Observable<TreeNode[]>; // Listado de datos a utilizar en el tree
+  @Input() field = 'label'; // Campo a mostrar en caso de utilizar objectos como valor en el autocomplete
 
   formControl: FormControl;
 
   filteredList$: Observable<TreeNode[]>;
-  searchSubject = new BehaviorSubject<string>('');
   suggestions$ = new BehaviorSubject<TreeNode[]>([]);
+
+  private searchSubject = new BehaviorSubject<string>(''); // Subject para el evento de búsqueda del autocomplete
 
   onChange = (_: any) => {};
   onTouch = () => {};
@@ -31,37 +35,44 @@ export class AutocompleteTreeComponent  implements ControlValueAccessor, OnInit 
   }
 
   ngOnInit(): void {
-    this.formControl = this.ngControl.control as FormControl;
+    this.formControl = this.ngControl.control as FormControl; // Obtenemos la instanacia del control del host (autocomplete-tree)
     this.filteredList$ = this.filteredList();
   }
 
+  // Método correspondiente al ControlValueAccessor
   writeValue(value: any): void {
     return value;
   }
 
+  // Método correspondiente al ControlValueAccessor
   registerOnChange(fn: any): void {
     this.onChange = fn;
   }
 
+  // Método correspondiente al ControlValueAccessor
   registerOnTouched(fn: any): void {
     this.onTouch = fn;
   }
 
+  // onSearch del autocomplete
   onSearch(event: AutocompleteEvent): void {
-    this.suggestions$.next([{}]);
-    this.searchSubject.next(event.query);
+    this.suggestions$.next([{}]); // Un único valor nos permite mostrar el template (el tree)
+    this.searchSubject.next(event.query); // Pasamos el término de búsqueda al Subject del evento
   }
 
+  // onNodeSelect del tree
   onNodeSelect(event: TreeEvent): void {
     const value = event.node;
-    this.suggestions$.next([value]);
-    this.formControl.setValue(value);
+    this.suggestions$.next([value]); // Pasamos el valor al autocomplete para que pueda ser mostrado
+    this.formControl.setValue(value); // Seteamos el valor del control con el nodo seleccionado en el tree
   }
 
+  // Evita la propagación del evento del nodo
   onStopPropagation(event: TreeEvent): void {
     event.originalEvent.stopPropagation();
   }
 
+  // Flujo para el filtrado de los datos del tree partiendo del término de búsqueda
   private filteredList(): Observable<TreeNode[]> {
     return this.searchSubject
       .pipe(
@@ -70,26 +81,33 @@ export class AutocompleteTreeComponent  implements ControlValueAccessor, OnInit 
       );
   }
 
-  private filterData(data: any, searchTerm: string, depth = 1): TreeNode[] {
+  /*
+   * Método recursivo para el filtrado de nodos en función del término de búsqueda.
+   * Si el nodo contiene el término de búsqueda, se muesta este y no se comprueban los hijos.
+   * Si el nodo no contiene el término de búsqueda, se comprueban en los hijos (si existen).
+   * Si los hijos contienen el término de búsqueda, se muestra la ruta completa con los padres expandidos
+   */
+  private filterData(data: TreeNode[], searchTerm: string): TreeNode[] {
     return data
-      .reduce((filteredData, row) => {
-        if (row.label && this.includes(row.label, searchTerm)) {
-          filteredData.push(row);
-        } else if (row.children?.length) {
-          const filteredChildren = this.filterData(row.children, searchTerm, ++depth);
+      .reduce((filteredData, node) => {
+        if (node.label && this.includes(node.label, searchTerm)) {
+          filteredData.push(node);
+        } else if (node.children?.length) {
+          const filteredChildren = this.filterData(node.children, searchTerm);
           if (filteredChildren?.length) {
-            filteredData.push({ ...row, children: filteredChildren, expanded: true });
+            filteredData.push({ ...node, children: filteredChildren, expanded: true });
           }
         }
         return filteredData;
-      }, [])
-      .flat(depth);
+      }, []);
   }
 
+  // Comprueba si una cadena contiene una subcadena sin tener en cuenta mayúscualas o acentos
   private includes(str: string, substr: string): boolean {
     return this.normalize(str).includes(this.normalize(substr));
   }
 
+  // Normaliza una cadena quitando acentos y pasándola a minúscula
   private normalize(str: string): string {
     return str.trim().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
   }
